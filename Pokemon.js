d7a81ab3c7e6e5bb842def7293d934431a09b200
PixelScoreMC <loganmcduffie@gmail.com>
2020-11-18 12:51:10 -0700

    Update Pokemon.js

--- Pokemon.js
+++ Pokemon.js
@@ -1,19 +1,23 @@
 const fetch = require('node-fetch');
 const fs = require('fs')
 
-let pokemon = "butterfree"
+let userInput = () => processFile(console.log(process.argv[2]), process.argv[2])
 
-function fetchStuff() {
+let processFile = (input) =>{
+    let inputText = fs.readFileSync(input)
+    let pokemonArray = inputText.split('\n')
+    pokemonArray.map(pokemon => fetchStuff(pokemon))
+    //here we find out what pokemon are in the list
+}
+
+
+let fetchStuff = (pokemon) => {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
         .then(response => response.json())
         .then(json => (json.types.map(element => element.type.name)))
         .then(data => console.log(pokemon + ": " + data.join(', ')))
-}   
+}
 
-fetchStuff();
+userInput()
 
 
-// 12: bug, flying
-// output: Butterfree: bug, flying
-
-//It worked!!
